be8f10cb479f5ed826c7019edcd0101e733cb09d
Pallapu Dileep Kumar <dileepkumarpallapu28@gmail.com>
2026-08-25 12:33:38 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (77.30%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,34 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+
+        while (mid <= high) {
+
+            if (nums[mid] == 0) {
+                // Swap 0 to the low side
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+
+                low++;
+                mid++;
+            }
+
+            else if (nums[mid] == 1) {
+                // 1 is already in the correct middle section
+                mid++;
+            }
+
+            else {
+                // Swap 2 to the high side
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+
+                high--;
+            }
+        }
+    }
+}
